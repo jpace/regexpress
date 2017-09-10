a39442a012ea94df3f16b5b3766351518343d8d2
Jeff Pace <jeugenepace@gmail.com>
2017-09-10 14:35:13 -0400

    Add factory to create regular expressions.

---
+++ test/regexpress/factory_test.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+require 'test/unit'
+require 'regexpress/factory'
+require 'paramesan'
+
+class RegexpFactoryTestCase < Test::Unit::TestCase
+  include Paramesan
+  
+  param_test [
+    [ "a\\z",     "a\\z"  ],
+    [ "a\\.z",    "a.z"  ],
+    [ "a\\.\\.z", "a..z" ],
+    [ "a\\.z.*",  "a.z*" ],
+    [ "a.z",      "a?z"  ],
+    [ "a\\$",     "a$"   ],
+    [ "a\\/z",    "a/z"  ],
+    [ "a\\(z\\)", "a(z)" ],
+  ].each do |exp, pat|
+    assert_equal exp, RegexpFactory.new.from_shell_pattern(pat), "pat: #{pat}"
+  end
+
+  param_test [
+    [ Regexp.new("a"), "a", Hash.new ],
+  ].each do |exp, pat, args|
+    assert_equal exp, RegexpFactory.new.create(pat, args), "pat: #{pat}; args: #{args}"
+  end
+end
+
+# class RegexpTestCase < Test::Unit::TestCase
+#   include Paramesan
+  
+#   def test_negated
+#     assert NegatedRegexp.new("a[b-z]").match("aa")
+#     assert NegatedRegexp.new(".+").match("")
+#   end
+  
+#   def test_invalid_whole_word
+#     assert_raises(RuntimeError) do
+#       Regexp.create ':abc', wholewords: true
+#     end
+#   end
+# end
